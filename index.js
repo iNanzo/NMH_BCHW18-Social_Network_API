a20ff53757bb36bcfc1e61b9e1f6515d838f79df
Newton Hoang <newtonhoang99@gmail.com>
2022-01-26 14:43:18 -0800

    clear controllers

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const routes = require('./routes');
 
 const cwd = process.cwd();
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
